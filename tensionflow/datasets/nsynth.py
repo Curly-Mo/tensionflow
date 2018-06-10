3d724bdcdfd92ebd5f436d387ac0740eda2398cf
Colin <colin@cfahy.com>
2018-06-10 17:14:47 -0400

    remove cyclomatic complexity

--- tensionflow/datasets/nsynth.py
+++ tensionflow/datasets/nsynth.py
@@ -16,15 +16,18 @@ class NSynth(datasets.Dataset):
         super().__init__(*args, **kwargs)
 
     def load_features_and_labels(self, split):
-        if split == 'train':
-            data_dir = os.path.join(self.data_dir, TRAIN_DIR)
-        if split == 'test':
-            data_dir = os.path.join(self.data_dir, TEST_DIR)
-        if split == 'valid':
-            data_dir = os.path.join(self.data_dir, VALID_DIR)
+        data_dir = self.split_dirs[split]
         audio_dir = os.path.join(data_dir, AUDIO_DIR)
         metadata_file = os.path.join(data_dir, METADATA_FILE)
         with open(metadata_file) as metadata_file:
             self.metadata = json.load(metadata_file)
         X, Y = zip(*[(os.path.join(audio_dir, f'{key}.wav'), meta) for key, meta in self.metadata.items()])
         return X, Y
+
+    @property
+    def split_dirs(self):
+        return {
+            'train':  os.path.join(self.data_dir, TRAIN_DIR),
+            'test': os.path.join(self.data_dir, TEST_DIR),
+            'validation': os.path.join(self.data_dir, VALID_DIR),
+        }
